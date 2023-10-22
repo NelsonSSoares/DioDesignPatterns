73e3f95d86db4baa899f28a8380d43caa2afc8c0
Nelson de Sousa Soares <nelsonssoares@ibm.com>
2023-10-22 08:48:21 -0300

    projeto finalizado

---
+++ src/dio/Test.java
@@ -0,0 +1,50 @@
+package dio;
+
+import dio.facade.Facade;
+import dio.singleton.SingletonEager;
+import dio.singleton.SingletonLazy;
+import dio.singleton.SingletonLazyHolder;
+import dio.strategy.*;
+
+public class Test {
+    public static void main(String[] args) {
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        // Strategy
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Nelson", "02040090");
+    }
+
+}
+
